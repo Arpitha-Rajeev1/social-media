31ca41f82ccec974fed845b7208cf3440a3c6d51
Arpitha <108493146+Arpitha-Rajeev1@users.noreply.github.com>
2023-04-22 08:39:31 +0200

    Third commit: Added router for user profile and installed ejs for views of home and user profile

--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const homeController = require('../controllers/home_controller');
 
 router.get('/', homeController.home);
+// for any further routes, access from here
+router.use('/users', require('./users'));
 
 module.exports = router;
